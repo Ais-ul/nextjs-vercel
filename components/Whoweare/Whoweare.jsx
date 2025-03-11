937f2256c2530d8cb50aa1ee03eb30e540f0debf
Ais-ul <mijarobert17@gmail.com>
2025-03-11 17:01:39 +0200

    rez  stele iphone

--- components/Whoweare/Whoweare.jsx
+++ components/Whoweare/Whoweare.jsx
@@ -21,7 +21,7 @@ export default function Whoweare() {
 
         <div className="flex justify-center ">
           {[...Array(5)].map((_, i) => (
-          <Star key={i} className="w-6 h-6" fill="currentColor" stroke="none" color="#f59e0b" />
+          <Star key={i} className="w-6 h-6 fill-amber-500" fill="currentColor" />
 
           ))}
         </div>
