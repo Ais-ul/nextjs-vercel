e493c66bf87df3dc9c0b55bcbbfcde0c534aece0
Ais-ul <mijarobert17@gmail.com>
2025-03-11 17:13:02 +0200

    push whoweare cu codul culorii

--- components/Whoweare/Whoweare.jsx
+++ components/Whoweare/Whoweare.jsx
@@ -8,16 +8,16 @@ export default function Whoweare() {
         Food Masters Romania – A New Standard for Food
         </h1>
 
-        <h2 className="text-amber-500 text-lg md:text-xl">Excellence in Catering, Perfection in Service</h2>
+        <h2 className="text-[#f59e0b] text-lg md:text-xl">Excellence in Catering, Perfection in Service</h2>
 
         <p className="text-gray-300 text-sm md:text-base leading-relaxed max-w-3xl mx-auto">
-          La <span className="text-amber-500">Food Masters</span>, we elevate catering through premium ingredients, 
-          innovative menus, and top-tier food safety standards. With <span className="text-amber-500">over 30 years of experience</span>, we provide customized meals for hospitals, 
+          La <span className="text-[#f59e0b]">Food Masters</span>, we elevate catering through premium ingredients, 
+          innovative menus, and top-tier food safety standards. With <span className="text-[#f59e0b]">over 30 years of experience</span>, we provide customized meals for hospitals, 
           schools, corporations, and diplomatic events, ensuring flawless service, fast delivery,
            and <span className="text-[#f59e0b]">exceptional quality in every dish.</span> 
         </p>
         <p className="text-gray-300 text-sm md:text-base leading-relaxed max-w-3xl mx-auto">We are more than just a catering provider - we are the trusted partner for those who 
-        demand <span className="text-amber-500"> perfection on every plate</span>. </p>
+        demand <span className="text-[#f59e0b]"> perfection on every plate</span>. </p>
 
         <div className="flex justify-center ">
           {[...Array(5)].map((_, i) => (
