7b58c139d2c16f012968667789e10b9dce67034a
Ais-ul <mijarobert17@gmail.com>
2025-03-11 18:32:42 +0200

    border-red-500

--- components/SolicitaOfertaModal/SolicitaOfertaModal.jsx
+++ components/SolicitaOfertaModal/SolicitaOfertaModal.jsx
@@ -36,7 +36,7 @@ export default function SolicitaOfertaModal({ isOpen, onClose }) {
               type="text"
               value={nume}
               onChange={(e) => setNume(e.target.value)}
-              className="w-full py-2 px-0  border-2 border-[#cda274] focus:outline-none"
+              className="w-full py-2 px-0  border-2 border-red-500 focus:outline-none"
               style={{ WebkitTextFillColor: "#cda274" }} // Adăugăm stilul inline
               placeholder=" Name"
               required
